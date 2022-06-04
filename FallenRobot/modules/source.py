97e62e02684563067b8b5a76bdc698667d2b7bb2
Cyberguy <aldrinsai007@gmail.com>
2022-06-04 20:02:57 +0530

    Update source.py

--- FallenRobot/modules/source.py
+++ FallenRobot/modules/source.py
@@ -7,7 +7,7 @@ from pyrogram import filters
 from FallenRobot import pbot as client
 
 
-ANON = "https://telegra.ph/file/7bd111132fce009e4605e.jpg"
+ANON = "https://telegra.ph/file/50a2d8a6acc66377cca50.jpg"
 
 @client.on_message(filters.command(["repo", "source"]))
 async def repo(client, message):
