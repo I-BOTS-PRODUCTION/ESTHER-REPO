7fec4528c62f3349956186ff9882195f2e57f41a
Cyberguy <aldrinsai007@gmail.com>
2022-06-06 01:04:32 +0530

    Update userinfo.py

--- FallenRobot/modules/userinfo.py
+++ FallenRobot/modules/userinfo.py
@@ -422,7 +422,7 @@ def set_about_me(update: Update, context: CallbackContext):
 @run_async
 @sudo_plus
 def stats(update: Update, context: CallbackContext):
-    stats = "<b>⫷ ᴇꜱᴛʜᴇʀ ᴄᴜʀʀᴇɴᴛ ꜱᴛᴀᴛɪꜱᴛɪᴄꜱ ⫸</b>\n" + "\n".join([mod.stats() for mod in STATS])
+    stats = "<b>⫷ ᴇꜱᴛʜᴇʀ ᴄᴜʀʀᴇɴᴛ ꜱᴛᴀᴛɪꜱᴛɪᴄꜱ ⫸</b>\n" + "\n".join([mod.__stats__() for mod in STATS])
     result = re.sub(r"(\d+)", r"<code>\1</code>", stats)
     update.effective_message.reply_text(result, parse_mode=ParseMode.HTML)
 
