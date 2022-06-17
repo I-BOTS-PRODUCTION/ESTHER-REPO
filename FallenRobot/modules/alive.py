8f7767774fb3e1681615f67eade11410ecbb54c8
I Bᴏᴛs Pʀᴏᴅᴜᴄᴛɪᴏɴ <aldrinsai007@gmail.com>
2022-06-17 14:46:43 +0530

    Update alive.py

--- FallenRobot/modules/alive.py
+++ FallenRobot/modules/alive.py
@@ -11,18 +11,25 @@ from FallenRobot import telethn as tbot
 
 
 PHOTO = [
-         "https://telegra.ph/file/27926d6cb3fa0e564b2b3.jpg",
-         "https://telegra.ph/file/74807aeea259acbc97fa1.jpg",
+    "https://telegra.ph/file/27926d6cb3fa0e564b2b3.jpg",
+    "https://telegra.ph/file/67d000677192dece279b2.jpg",
 ]
 
 @register(pattern=("/alive"))
 async def awake(event):
- TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ Ξ S Γ H Ξ Я​**\n━━━━━━━━━━━━━━━━━━━\n\n"
- TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝗤𝗨𝗥𝗘𝗦𝗛𝗜](https://t.me/BROTHER_OF_VILLAIN)** \n\n"
- TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
- TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
- TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
- BUTTON = [[Button.url("★ʜᴇʟᴘ★", "https://t.me/Missestherbot?start=help"), Button.url("★ꜱᴜᴘᴘᴏʀᴛ★", "https://t.me/ibotssupport")]]
-
-ran = random.choice(PHOTO)
+    TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ Ξ S Γ H Ξ Я​**\n━━━━━━━━━━━━━━━━━━━\n\n"
+    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝗤𝗨𝗥𝗘𝗦𝗛𝗜](https://t.me/Brother_of_villain)** \n\n"
+    TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
+    TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
+    TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
+    BUTTON = [
+        [
+            Button.url("★ʜᴇʟᴘ★​", "https://t.me/MissEstherBot?start=help"),
+            Button.url("★sᴜᴘᴘᴏʀᴛ​★", "https://t.me/ibotssupport"),
+        ]
+    ]
+    ran = random.choice(PHOTO)
     await tbot.send_file(event.chat_id, ran, caption=TEXT, buttons=BUTTON)
+
+
+## Alive mod
